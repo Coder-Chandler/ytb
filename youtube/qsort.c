86d1788f25cc8ec32f46c61df38d2e244da81da6
Chandler <ysiwgtus@gmail.com>
2017-05-16 21:21:45 +0800

    C

--- youtube/qsort.c
+++ youtube/qsort.c
@@ -13,3 +13,6 @@ int main()
     qsort(A,6,sizeof(int),compare);
     for (i = 0; i<6; i++) printf ("%d\n", A[i])
 }
+/*qsort 的函数原型是void __cdecl qsort ( void *base, size_t num, size_t width, int (__cdecl *comp)(const void *, const void* ) ) 
+其中base是排序的一个集合数组，num是这个数组元素的个数，width是一个元素的大小，comp是一个比较函数。
+*/
