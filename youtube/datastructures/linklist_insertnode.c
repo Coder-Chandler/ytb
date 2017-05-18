8b4a194a99e13b08db9ba2637a2ec2fb3142d7ab
Chandler <ysiwgtus@gmail.com>
2017-05-19 01:01:14 +0800

    change the format

--- youtube/datastructures/linklist_insertnode.c
+++ youtube/datastructures/linklist_insertnode.c
@@ -1,36 +1,43 @@
 //Link List in C/C++ insert a node at nth position
 #include <stdio.h>
 #include <stdlib.h>
-struct Node {
+struct Node 
+{
     int data;
     struct Node *next;
 };
 struct Node *head;//Global variable
-void print() {
+void print() 
+{
     Node *temp = head;
-    while (temp != NULL) {
+    while (temp != NULL) 
+    {
         printf ("%d ",temp -> data);
         temp = temp -> next;
     }
     printf ("\n");
 }
-void Insert (int data, int n) {
-    Node *temp1 = (int*)malloc(sizeof(struct Node));
+void Insert (int data, int n) 
+{
+    Node *temp1 = (struct Node*)malloc(sizeof(struct Node));
     temp1 -> data = data;
     temp1 -> next = NULL;
-    if (n == 1) {
+    if (n == 1) 
+    {
         temp1 -> next = head;
         head = temp1;
         return;
     }
     Node *temp2 = head;
-    for (int i = 0; i<n-2; i++) {
+    for (int i = 0; i<n-2; i++) 
+    {
         temp2 = temp2 -> next;
     }
     temp1 -> next = temp2 -> next;
     temp2 -> next = temp1;
 }
-int main() {
+int main() 
+{
     head = NULL;//empty list
     Insert(2,1);
     Insert(3,2);
